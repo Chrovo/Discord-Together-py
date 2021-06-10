a3583e3655e7ebdca46e23194b60694d25fd3134
Chrovo <chrovo1@gmail.com>
2021-06-10 18:05:28 -0400

    Updated setup.py by adding long_description

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
         author="Chrovo",
         version="0.0.1",
         description = "This is a package that is ideally used with discord.py, it allows you to do different activities in voice chat",
-        #long_description = open("README.md").read(),
+        long_description = "This is a package designed for discord bots, its meant for you to be able to watch youtube together in a voice channel and do other activities together. This is inspired from RemyK888's discord together but for javascript. However, this package is for python. Ideally used with the discord.py wrapper.",
         url = "https://github.com/Chrovo/Discord-Together-py",
         classifiers = ["Intended Audience::Developers", "Programming Language::Python", "Natural Language::English","Programming Languuage::Python::3"],
         install_requires = ['discord.py','aiohttp'],
         license = "MIT",
         packages = find_packages(),
-        keywords="discord-together"
+        keywords="discord-together",
 )
