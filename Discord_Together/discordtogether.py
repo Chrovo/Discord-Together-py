7c7e89737714aea43ea0c47ab844fea52aa06aac
Chrovo <83519535+Chrovo@users.noreply.github.com>
2022-05-07 00:44:28 -0400

    Make code more readable and better
    Makes code better by using `tuples`(better for memory), consistent quotation marks, and adding a `session` parameter in case users already have a `ClientSession` instance.

--- Discord_Together/discordtogether.py
+++ Discord_Together/discordtogether.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Optional
 
 import aiohttp
 import discord
@@ -24,18 +25,23 @@ class DiscordTogether:
         1.) option:str - This is a kwarg that takes in the discord together option you chose.(This is a keyword argument).
         2.) vc_id:int - This is the voice channel id we need for the discord together activity to function.(This is a keyword argument).
     """
-    def __init__(self, *, token: str) -> None:
+    def __init__(
+        self, *, 
+        token: str, 
+        session: Optional[aiohttp.ClientSession] = None
+    ) -> None:
         self.token = token
+        self.session = session or aiohttp.ClientSession()
 
     async def activity(
         self, *,
         option: str,
         vc_id: int
-    ):
+    ) -> str:
 
-        ALL_OPTIONS = ["youtube", "poker", "betrayal", "fishing", "chess"]
+        ALL_OPTIONS = ('youtube', 'poker', 'betrayal', 'fishing', 'chess',)
         CONVERSIONS = {
-            "youtube": "755600276941176913",  # Credit goes to RemyK888 for all of these ids, thanks.
+            'youtube': '755600276941176913',  # Credit goes to RemyK888 for all of these ids, thanks.
             'poker':'755827207812677713',
             'betrayal': '773336526917861400',
             'fishing': '814288819477020702',
@@ -46,29 +52,26 @@ class DiscordTogether:
             raise InvalidOptionError(f"Invalid option '{option}' provided")
         else:
             opt_id = CONVERSIONS[option.lower()]
-
-            async with aiohttp.ClientSession() as cs:
-                async with cs.post(f"https://discord.com/api/v8/channels/{vc_id}/invites",
-                    json={
-                        "max_age": 86400,
-                        "max_uses": 0,
-                        "target_application_id": opt_id,
-                        "target_type": 2,
-                        "temporary": False,
-                        "validate": None,
-                    }, headers={
-                        "Authorization": f"Bot {self.token}",
-                        "Content-Type": "application/json",
+            async with self.session.post(f"https://discord.com/api/v8/channels/{vc_id}/invites",
+                json={
+                    'max_age': 86400,
+                    'max_uses': 0,
+                    'target_application_id': opt_id,
+                    'target_type': 2,
+                    'temporary': False,
+                    'validate': None,
+                }, headers={
+                    'Authorization': f'Bot {self.token}',
+                    'Content-Type': 'application/json',
                     },
-                ) as r:
-
-                    if r.status in range(200, 300):
-                        data = await r.json()
-                        invitecode = data['code']
-                        return invitecode
-                    elif r.status == 401:
-                        raise InvalidTokenError("Invalid token was provided")
-                    elif r.status == 400:
-                        raise InvalidVoiceChannelError(f"Invalid voice channel id '{vc_id}' provided")
-                    else:
-                        raise HTTPConnectionError(f"Connection was unsuccessful: {r.status}:{r.reason}")
+            ) as r:                  
+                if r.status in range(200, 300):
+                    data = await r.json()
+                    invite_code = data['code']
+                    return invite_code
+                elif r.status == 401:
+                    raise InvalidTokenError('Invalid token was provided')
+                elif r.status == 400:
+                    raise InvalidVoiceChannelError(f'Invalid voice channel id '{vc_id}' provided')
+                else:
+                    raise HTTPConnectionError(f'Connection was unsuccessful: {r.status}:{r.reason}')
